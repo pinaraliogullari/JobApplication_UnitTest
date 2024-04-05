cb92bea05c7f1ffd8b32fd55adffcdb58c378a8c
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-04-05 15:02:21 +0300

    File created

---
+++ JobApplication/JobApplicationLibrary/ApplicationEvaluator.cs
@@ -0,0 +1,24 @@
+﻿using JobApplicationLibrary.Models;
+
+namespace JobApplicationLibrary
+{
+	public class ApplicationEvaluator
+	{
+		private readonly int minAge = 18;
+		public ApplicationResult Evaluate(JobApplication form)
+		{
+			if (form.Applicant.Age < minAge)
+				return ApplicationResult.AutoRejected;
+			return ApplicationResult.AutoAccepted;
+		}
+	}
+	public enum ApplicationResult
+	{
+		AutoRejected,
+		TransferredToHR,
+		TransferredToLead,
+		TransferredToCTO,
+		AutoAccepted
+	}
+	
+}
